30190d59a9d7961054391567391ea277f0018011
Aaron <pioneervon@gmail.com>
2016-06-08 09:46:29 +0800

    Change exe filter to filter extention rather than filter install.bat
    Signed-off-by: Aaron <pioneervon@gmail.com>

--- BAT/AutoInstall/customize_prefs/install.bat
+++ BAT/AutoInstall/customize_prefs/install.bat
@@ -1,11 +1,10 @@
 @echo off
-set mfilter=install.bat
 
 for /f "delims=" %%a in ('dir /b/a-d/od') do (
-if not %%a==%mfilter% (
-echo installing %%a
-start %%a
-)
+	if "%%~xa"==".exe" (
+		echo installing %%a
+		start %%a
+	)
 )
 
 pause
